b93b594fae7f5d655632e7da5fd8b90e0bfdbfbf
Clyde1063 <clyde.apples@gmail.com>
2022-07-05 16:48:24 +0200

    added login feature with zustand (pretty cool)

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -15,7 +15,7 @@ const Discover = () => {
 
   return (
     <div className='xl:border-b-2 xl:border-gray-200 pb-6 '>
-      <p className='text-gray-500 font-semibold m-3 mt-4 xl:block'>
+      <p className='text-gray-500 font-semibold m-3 mt-4 hidden xl:block'>
         Popular topics
         </p>
         <div className="flex gap-3 flex-wrap">
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,32 +2,52 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import {useRouter} from 'next/router'
-import { GoogleLogin, GoogleLogout } from 'react-google-login'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import {AiOutlineLogout} from 'react-icons/ai'
 import {BiSearch} from 'react-icons/bi'
 import {IoMdAdd} from 'react-icons/io'
 import {BiVideo} from 'react-icons/bi'
+import {GoogleLogin, googleLogout} from '@react-oauth/google'
+import useAuthStore from '../store/authStore'
 
 import Logo from '../utils/tiktik-logo.png';
+import { createOrGetUser } from '../utils'
 
 const Navbar = () => {
+  const {userProfile, addUser} = useAuthStore()
   return (
-    <div className="w-full flex justify-between border-b-2 border-gray-200 py-2 px-4">
-      <Link href="/">
-         <div className="w-[100px] md:w-[130px]">
-          {/* test Image */}
-            {/* <Image 
-               className="cursor-pointer"
-               src={Logo}
-              alt="logo"
-              layout="responsive"
-            /> */}
-            <div className="text-4xl">
-              <h1 className="flex">Vidly<BiVideo  className="text-5xl"/></h1>
-            </div>
-         </div>
-      </Link>
-    </div>
+
+    <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}>
+      <div className="w-full flex justify-between border-b-2 border-gray-200 py-2 px-4">
+        <Link href="/">
+           <div className="w-[100px] md:w-[130px]">
+            {/* test Image */}
+              {/* <Image
+                 className="cursor-pointer"
+                 src={Logo}
+                alt="logo"
+                layout="responsive"
+              /> */}
+              <div className="text-4xl">
+                <h1 className="flex">Vidly<BiVideo  className="text-5xl"/></h1>
+              </div>
+           </div>
+        </Link>
+        <div>
+          search
+        </div>
+        <div>
+          {userProfile ? (
+            <div>{userProfile.userName}</div>
+          ):(
+            <GoogleLogin 
+            onSuccess={(response) => createOrGetUser(response, addUser)}
+            onError={() => console.log("Error")}
+            />
+          )}
+        </div>
+      </div>
+    </GoogleOAuthProvider>
   )
 }
 
