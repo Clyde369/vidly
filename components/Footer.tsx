e53949d8f6e80fd1c6ebe7aad8cf340f9e3620db
Clyde1063 <clyde.apples@gmail.com>
2022-07-05 15:09:08 +0200

    added backend and videoCard and NoResults comp

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,8 +3,8 @@ import {footerList1, footerList2, footerList3} from '../utils/constants'
 
 const d = new Date();
 
-const List = ({items}: {items: string[]}) => (
-  <div className="flex flex-wrap gap-2 mt-5">
+const List = ({items, mt}: {items: string[], mt: boolean}) => (
+  <div className={`flex flex-wrap gap-2 ${mt && 'mt-5'}`}>
     {items.map((item) => (
       <p key={item} className="text-gray-400 text-em hover:underline cursor-pointer">
         {item}
@@ -16,10 +16,10 @@ const List = ({items}: {items: string[]}) => (
 export const Footer = () => {
   return (
     <div className="mt-6 hidden xl:block">
-      <List items={footerList1}/>
-      <List items={footerList2}/>
-      <List items={footerList3}/>
-      <p>Vidly {d.getFullYear()}</p>
+      <List items={footerList1} mt={false}/>
+      <List items={footerList2} mt/>
+      <List items={footerList3} mt/>
+      <p className="text-gray-500 text-sm mt-5">Vidly {d.getFullYear()}</p>
     </div>
   )
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,6 +6,7 @@ import { GoogleLogin, GoogleLogout } from 'react-google-login'
 import {AiOutlineLogout} from 'react-icons/ai'
 import {BiSearch} from 'react-icons/bi'
 import {IoMdAdd} from 'react-icons/io'
+import {BiVideo} from 'react-icons/bi'
 
 import Logo from '../utils/tiktik-logo.png';
 
@@ -14,12 +15,16 @@ const Navbar = () => {
     <div className="w-full flex justify-between border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
          <div className="w-[100px] md:w-[130px]">
-            <Image 
+          {/* test Image */}
+            {/* <Image 
                className="cursor-pointer"
                src={Logo}
               alt="logo"
               layout="responsive"
-            />
+            /> */}
+            <div className="text-4xl">
+              <h1 className="flex">Vidly<BiVideo  className="text-5xl"/></h1>
+            </div>
          </div>
       </Link>
     </div>
